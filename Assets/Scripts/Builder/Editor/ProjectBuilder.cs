72a8e0a51827e48004593c44466f5bb12b296484
LovelyPenguin <lyp3238@gmail.com>
2019-03-28 02:01:27 +0900

    빌드 스크립트 수정
    - 함수를 잘못 적음

--- Assets/Scripts/Builder/Editor/ProjectBuilder.cs
+++ Assets/Scripts/Builder/Editor/ProjectBuilder.cs
@@ -79,7 +79,7 @@ public class ProjectBuilder
         EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, build_target);
         UnityEditor.Build.Reporting.BuildReport res = BuildPipeline.BuildPlayer(scenes, target_path, build_target, build_options);
 
-        char sep = Path.AltDirectorySeparatorChar;
+        char sep = Path.DirectorySeparatorChar;
         string REPORT_TARGET_PATH = Path.GetFullPath(".") + sep + "/BuildReport";
         // 빌드 번호 설정해줘야 함.
         BuildReportMaker buildReportMaker = new BuildReportMaker(buildReportFileName, res, REPORT_TARGET_PATH);
