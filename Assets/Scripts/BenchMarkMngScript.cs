187c70289feeca591bbb049d950483263437eef8
LovelyPenguin <lyp3238@gmail.com>
2019-04-08 21:15:29 +0900

    제한시간까지만 프레임을 기록하도록 수정함

--- Assets/Scripts/BenchMarkMngScript.cs
+++ Assets/Scripts/BenchMarkMngScript.cs
@@ -9,10 +9,10 @@ public class BenchMarkMngScript : MonoBehaviour
     private TextMeshProUGUI frameRateText;
     [SerializeField]
     private TextMeshProUGUI elapsedTimeText;
-	[SerializeField]
-	private TextMeshProUGUI deviceNameText;
-	[SerializeField]
-	private TextMeshProUGUI frameLogText;
+    [SerializeField]
+    private TextMeshProUGUI deviceNameText;
+    [SerializeField]
+    private TextMeshProUGUI frameLogText;
 
     private float currentTime;
     private float currentFrame;
@@ -26,40 +26,41 @@ public class BenchMarkMngScript : MonoBehaviour
     private float targetFrameRate;
     void Start()
     {
-		Debug.Log("BenchMark Kit Start");
+        Debug.Log("BenchMark Kit Start");
         if (frameRateText == null || elapsedTimeText == null)
         {
             Debug.LogError("frameRate or ElapsedTime가 비어있습니다.");
         }
-		deviceNameText.text = 
-		"Device Name : " + SystemInfo.deviceModel + "\n" + 
-		"Processor : " + SystemInfo.processorType + "\n" +
-		"Memory Size : " + SystemInfo.systemMemorySize + "\n" +
-		"Graphics Processor : " + SystemInfo.graphicsDeviceName + "\n" +
-		"Graphics Memory Size : " + SystemInfo.graphicsMemorySize;
-		currentFrame = 0f;
-		currentTime = 0f;
+        deviceNameText.text =
+        "Device Name : " + SystemInfo.deviceModel + "\n" +
+        "Processor : " + SystemInfo.processorType + "\n" +
+        "Memory Size : " + SystemInfo.systemMemorySize + "\n" +
+        "Graphics Processor : " + SystemInfo.graphicsDeviceName + "\n" +
+        "Graphics Memory Size : " + SystemInfo.graphicsMemorySize;
+        currentFrame = 0f;
+        currentTime = 0f;
     }
 
     void Update()
     {
-		currentFrame = (int)(1f / Time.unscaledDeltaTime);
-		frameRateText.text = currentFrame.ToString();
+        if (currentTime < targetTime)
+            currentFrame = (int)(1f / Time.unscaledDeltaTime);
+        frameRateText.text = currentFrame.ToString();
 
-		currentTime += Time.deltaTime;
-		elapsedTimeText.text = currentTime.ToString();
+        currentTime += Time.deltaTime;
+        elapsedTimeText.text = currentTime.ToString();
 
-		if (currentTime >= targetTime)
-		{
-			Time.timeScale = 0f;
-			Debug.Log("Test End");
-		}
-		if (currentFrame < targetFrameRate && currentTime >= 2f)
-		{
-			string tempText;
-			tempText = "Time : " + currentTime + "\n" + "Frame : " + currentFrame + "\n" + "-----------------" + "\n";
-			frameLogText.text += tempText;
-			Debug.LogWarning(tempText);
-		}
+        if (currentTime >= targetTime)
+        {
+            Time.timeScale = 0f;
+            Debug.Log("Test End");
+        }
+        if (currentFrame < targetFrameRate && currentTime >= 2f)
+        {
+            string tempText;
+            tempText = "Time : " + currentTime + "\n" + "Frame : " + currentFrame + "\n" + "-----------------" + "\n";
+            frameLogText.text += tempText;
+            Debug.LogWarning(tempText);
+        }
     }
 }
