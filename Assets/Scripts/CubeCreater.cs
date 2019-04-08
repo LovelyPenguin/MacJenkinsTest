c66d4bf65a6a2921215593b4653ac6328bf0ce88
LovelyPenguin <lyp3238@gmail.com>
2019-04-08 17:52:48 +0900

    벤치마크용 씬 추가

---
+++ Assets/Scripts/CubeCreater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCreater : MonoBehaviour
+{
+
+    [SerializeField]
+    private float time = 1f;
+    private float saveTime;
+
+    [SerializeField]
+    private GameObject cube;
+	[SerializeField]
+	private Transform cubeSave;
+
+    void Start()
+    {
+        if (cube == null)
+        {
+			Debug.LogError("Cube is null");
+        }
+    }
+
+    void Update()
+    {
+        saveTime += Time.deltaTime;
+
+        if (saveTime >= time && cube != null)
+        {
+            Instantiate(cube, transform.position, Quaternion.identity, cubeSave);
+            saveTime = 0f;
+        }
+    }
+}
