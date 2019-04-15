a50cc45aa0c57d448bd5397b6e2e963ab42a93fa
LovelyPenguin <lyp3238@gmail.com>
2019-04-15 20:41:02 +0900

    이해 안가는 문제가 자꾸 생김

--- Assets/Scripts/Builder/Editor/GetCSVData.cs
+++ Assets/Scripts/Builder/Editor/GetCSVData.cs
@@ -35,8 +35,8 @@ public class GetCSVData
         using (StreamWriter outputFile = new StreamWriter(csvFilePath, true))
         {
             // outputFile.WriteLine("{0},{1},{2}", System.DateTime.Now.ToString("yyyy.MM.dd") + System.DateTime.Now.ToString("(HH:mm:ss)"), setBundleVersionCode + 1, setVersion);
-            int tempBundleVersion = setBundleVersionCode+1;
-            outputFile.WriteLine("{0},{1},{2}", System.DateTime.Now.ToString("yyyy.MM.dd") + System.DateTime.Now.ToString("(HH:mm:ss)"), setBundleVersionCode+1, setBundleVersionCode+1);
+            //int tempBundleVersion = setBundleVersionCode+1;
+            outputFile.WriteLine("{0},{1},{2}", System.DateTime.Now.ToString("yyyy.MM.dd") + System.DateTime.Now.ToString("(HH:mm:ss)"), setBundleVersionCode+1, setVersion);
         }
         // 코드입력
 
